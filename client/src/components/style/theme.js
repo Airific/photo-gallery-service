eda5ffd74d460ef41238cf56d784528004b215c5
Victoria Chen <vicchen12@gmail.com>
2020-09-29 11:39:00 -0700

    change font to airbnb font to match

--- client/src/components/style/theme.js
+++ client/src/components/style/theme.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Theme = styled.div`
-  font-family: 'Roboto', sans-serif;
+  font-family: Circular, -apple-system, system-ui, Roboto, "Helvetica Neue", sans-serif;
 `;
 
 export default Theme;
