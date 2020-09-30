cdda850e1e78719e337abe0f88d99da13b2e851d
Victoria Chen <vicchen12@gmail.com>
2020-09-29 18:09:39 -0700

    Change outline to none for all buttons

--- client/src/components/style/theme.js
+++ client/src/components/style/theme.js
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 const Theme = styled.div`
   font-family: Circular, -apple-system, system-ui, Roboto, "Helvetica Neue", sans-serif;
+
+  button {
+    outline: none;
+  }
 `;
 
 export default Theme;
